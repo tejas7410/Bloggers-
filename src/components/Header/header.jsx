10fdd299c9eed424b92ded04e8ba544ced2c2bd6
tejas7410 <img_2021030@iiitm.ac.in>
2025-05-20 15:59:00 +0530

    added Post Card, Logo box, select for selecting post and getting posts, added forward reference to inegrate the input and UI and semelessly preview the posts

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,11 +6,47 @@ import { useNavigate } from 'react-router-dom'
 
 const header = () => {
     const authstatus = useSelector((state) => { state.auth.status })
-    console.log(authstatus)
+    const navigate = useNavigate()
+    const navItems = [
+        { name: "Home", path: "/", active: true },
+        { name: "Login", path: "/login", active: !authstatus },
+        { name: "Signup", path: "/signup", active: !authstatus },
+        { name: "All Posts", path: "/all-post", active: authstatus },
+        { name: "Add Post", path: "/add-post", active: authstatus },
+
+    ]
+
+
     return (
-        <div>
-            Header
-        </div>
+        <header className='py-3 bg-gray-500 shadow'>
+            <Container>
+                <nav className='flex'>
+                    <div className='mr-4'>
+                        <Link to='/'>
+                            <Logo width='70px' />
+
+                        </Link>
+                    </div>
+                    <ul className='flex ml-auto'>
+                        {navItems.map((item) =>
+                            item.active ? (
+                                <li key={item.name}>
+                                    <button
+                                        onClick={() => navigate(item.slug)}
+                                        className='inline-bock hover:bg-blue-100 px-6 py-2 duration-200 rounded-full'
+                                    >{item.name}</button>
+                                </li>
+                            ) : null
+                        )}
+                        {authstatus && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
     )
 }
 
---
+++ src/components/Select.jsx
@@ -0,0 +1,32 @@
+import { useId } from "react"
+import React from 'react'
+
+function Select(
+    {
+        options,
+        label,
+        className = "",
+        ...props
+    }, ref
+) {
+    return (
+        <div className="w-full">
+            {label && <label htmlFor={id} className=''>{label}</label>}
+            <select
+                {...props}
+                ref={ref}
+                id={id}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            >
+                {options?.map((option) => (
+                    <option key={option} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+
+            </select>
+        </div>
+    )
+}
+
+export default React.forwardRef(Select)

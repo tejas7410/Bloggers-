8c347c7567bd16db66921b17d230635c3d7bacc8
tejas7410 <img_2021030@iiitm.ac.in>
2025-05-22 02:53:38 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { AuthLayout, Login } from './components/index.js'
 
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
+import Signup from './pages/SignUp'
 import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
